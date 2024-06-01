b1783d8e87f9958292106b674c4c9e0856e1bb6f
wppaing <wintphyopaing.p@gmail.com>
2024-06-01 22:25:22 +0630

    Structs

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!!!");
+    let mut s1 = String::from("Hello");
+
+    let s2 = takes_and_gives_back(&mut s1);
+
+    println!("{}, {}", s1, s2);
+}
+
+fn takes_and_gives_back(s: &mut String) -> usize {
+    s.push_str("new string");
+    s.len()
 }
